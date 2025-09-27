67d3add13d51adf36bf3a225207953cbcef8188b
Alejandro <alecoco09j@gmail.com>
2025-09-27 15:27:17 -0400

    lil botunm

---
+++ next-step/app/practice/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TestPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <a
+        href="/"
+        className="bg-white hover:bg-gray-300 text-black text-sm font-medium px-4 py-2 rounded-md"
+      >
+        Home
+      </a>
+    </div>
+  );
+}
