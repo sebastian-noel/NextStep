ec473b4d1982f5644e5374deb484696d8c2722c5
Alejandro <alecoco09j@gmail.com>
2025-09-27 15:33:19 -0400

    big  botunm

--- next-step/app/practice/page.tsx
+++ next-step/app/practice/page.tsx
@@ -5,9 +5,9 @@ export default function TestPage() {
     <div className="flex justify-center items-center min-h-screen bg-black">
       <a
         href="/"
-        className="bg-white hover:bg-gray-300 text-black text-sm font-medium px-4 py-2 rounded-md"
+        className="bg-green-500 hover:bg-green-700 text-black text-sm font-medium px-10 py-10 rounded-md"
       >
-        Home
+        Speak with AI!
       </a>
     </div>
   );
