7d4052ec805e350f47ae8a148a1dfa05fed51557
Stevin006 <Steving2006@gmail.com>
2025-09-27 14:18:11 -0400

    init commit

--- next-step/app/page.tsx
+++ next-step/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
+            lalalalalal
           </li>
         </ol>
 
