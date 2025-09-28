7333cebdd9bfc095735bed0bcdf2cec46d1798d8
Carlos Rincon <carincon06@gmail.com>
2025-09-28 00:05:24 -0400

    New header for type of insurance

--- next-step/app/page.tsx
+++ next-step/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <section className="px-6 pb-16">
         <div className="max-w-6xl mx-auto">
           <h3 className="text-2xl md:text-3xl font-bold text-center text-gray-800 mb-6 mt-3">
-            Choose Your Insurance Type
+            Choose What Insurance To Practice
           </h3>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <Link 
